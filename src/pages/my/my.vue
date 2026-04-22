33f7516678fed0023fa5f7c7831eadbaad60eb45
laixiaobo <1483628145l@gmail.com>
2026-04-22 16:25:48 +0800

    完成请求拦截器

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
 
+import '@/utils/http'
+
 const memberStore = useMemberStore()
+
+const getApi = () => {
+  uni.request({
+    url: '/home/banner',
+    method: 'GET',
+  })
+}
 </script>
 
 <template>
@@ -21,16 +30,8 @@ const memberStore = useMemberStore()
       保存用户信息
     </button>
     <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="getApi" size="mini" plain type="primary">请求测试</button>
   </view>
-
-  <uni-card
-    title="基础卡片"
-    sub-title="副标题"
-    extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-  >
-    <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-  </uni-card>
 </template>
 
 <style lang="scss">
--- src/stores/modules/member.ts
+++ src/stores/modules/member.ts
@@ -5,17 +5,21 @@ import { ref } from 'vue'
 export const useMemberStore = defineStore(
   'member',
   () => {
-    // 会员信息
-    const profile = ref<any>()
+    // 会员信息 token
+    const profile = ref<any>(uni.getStorageSync('memberProfile'))
 
     // 保存会员信息，登录时使用
     const setProfile = (val: any) => {
       profile.value = val
+      // 配置数据持久化
+      uni.setStorageSync('memberProfile', val)
     }
 
     // 清理会员信息，退出时使用
     const clearProfile = () => {
       profile.value = undefined
+      // 清除数据持久化
+      uni.removeStorageSync('memberProfile')
     }
 
     // 记得 return
