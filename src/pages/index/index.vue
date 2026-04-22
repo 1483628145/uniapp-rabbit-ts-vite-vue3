3f6fcb880013fd6e78f862c2b721815aa6714bd6
laixiaobo <1483628145l@gmail.com>
2026-04-22 17:03:32 +0800

    完成首页导航条的设置

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-//
+import customNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
+  <customNavbar></customNavbar>
   <view class="index">index</view>
 </template>
 
